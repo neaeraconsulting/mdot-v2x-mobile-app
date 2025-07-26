5ddcb20f06d6e7ac3440f3765d077692ab5e0b47
John-Wiens <jwiens3141@gmail.com>
2025-07-26 12:36:44 -0600

    Update map_page.dart

--- cv_mec/lib/pages/map_page.dart
+++ cv_mec/lib/pages/map_page.dart
@@ -185,7 +185,7 @@ class MapState extends State<MapPage> {
   late Image currentLightState;
   late String nextLightText = "";
 
-  bool debugMode = true;
+  bool debugMode = false;
   bool showLoadingIcon = true;
   bool showLightText = true;
 
