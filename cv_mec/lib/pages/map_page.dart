0c262406c28f362369b47f0a9fe292197cf74c5a
john-wiens <jwiens3141@gmail.com>
2025-08-14 10:02:49 -0600

    Undoing Debug Mode

--- cv_mec/lib/pages/map_page.dart
+++ cv_mec/lib/pages/map_page.dart
@@ -189,7 +189,7 @@ class MapState extends State<MapPage> {
   late Image currentLightState;
   late String nextLightText = "";
 
-  bool debugMode = true;
+  bool debugMode = false;
   bool showLoadingIcon = true;
   bool showLightText = true;
 
