e01b266cc860da7416806491c289e5bf155cc32a
john-wiens <jwiens3141@gmail.com>
2025-10-15 09:40:18 -0600

    Added source field to TIM messages

--- cv_mec/lib/models/tim_definition.dart
+++ cv_mec/lib/models/tim_definition.dart
@@ -5,12 +5,14 @@ import 'package:cv_mec/models/text_overlay.dart';
 class TimDefinition {
   late String name;
   late String type;
+  late String source;
   late List<String> codes;
   late String graphic;
   List<TextOverlay> overlays = [];
 
   TimDefinition({
     required this.name,
+    required this.source,
     required this.type,
     required this.codes,
     required this.graphic,
@@ -20,6 +22,7 @@ class TimDefinition {
   factory TimDefinition.fromJson(Map<String, dynamic> json) {
     return TimDefinition(
       name: json['name'] ?? '',
+      source: json['source'] ?? '',
       type: json['type'] ?? '',
       codes: (json['codes'] as List<dynamic>?)
               ?.map((e) => e.toString())
@@ -37,18 +40,10 @@ class TimDefinition {
   // Convert back to JSON
   Map<String, dynamic> toJson() => {
     'name': name,
+    'source': source,
     'type': type,
     'codes': codes,
     'graphic': graphic,
     'overlays': overlays.map((e) => e.toJson()).toList(),
   };
 }
-
-  // factory TimDefinition.fromJson(Map<String, dynamic> json) {
-  //   TimDefinition tim = TimDefinition();
-  //   tim.name = json['name'];
-  //   tim.type = json['type'];
-  //   tim.codes = List<String>.from(json['codes']);
-  //   tim.graphic = json['graphic'];
-  //   tim.overlays = [];
-  // }
