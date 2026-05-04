edca3eda9032b0bf964447ae5ac43cd0ddcd9dfe
AnyaWiens <46979056+AnyaWiens@users.noreply.github.com>
2026-05-04 16:21:35 -0600

    Added comment for clarity

--- cv_mec/lib/models/mqtt/mqtt_agent_manager.dart
+++ cv_mec/lib/models/mqtt/mqtt_agent_manager.dart
@@ -11,7 +11,7 @@ class MqttAgentManager{
   MqttAgentManager(){}
 
   Future<int> connectAll() async{
-    for(MqttAgent agent in List<MqttAgent>.from(agents)){
+    for(MqttAgent agent in List<MqttAgent>.from(agents)){ //looping this way to avoid concurrent modification errors
       logger.i("Connecting Agent ${agent.agentName}");
       final success = await agent.connect();
       if(success != 0){
